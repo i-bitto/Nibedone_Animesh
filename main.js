f122fbbfa4bfeda53d3f9d773dede1972638ca5f
i-bitto <i.bittosaha@gmail.com>
2024-08-01 18:07:35 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Hello World!');
+function scrollToSection(sectionId) {
+            var element = document.getElementById(sectionId);
+            if (element) {
+                element.scrollIntoView({ behavior: "smooth" });
+            } else {
+                console.error("Element with id '" + sectionId + "' not found.");
+            }
+}
